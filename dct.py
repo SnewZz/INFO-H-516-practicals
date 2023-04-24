7b90fbfb7cd1e712e1966102e4bf256f898df4f5
SnewZz <64595565+SnewZz@users.noreply.github.com>
2023-04-24 14:53:16 +0200

    tmp commit

---
+++ dct.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import dct, idct
+from PIL import Image
+import graphlearning as gl
+
+def dct2(f):
+    """2D Discrete Cosine Transform
+
+    Args:
+        f: Square array
+
+    Returns: 
+        2D DCT of f
+    """
+    return dct(dct(f, axis=0, norm='ortho' ),axis=1, norm='ortho')
+
+
+
+
+def idct2(f):
+    """2D Inverse Discrete Cosine Transform
+
+    Args:
+        f: Square array
+
+    Returns: 
+        2D Inverse DCT of f
+    """
+    return idct(idct(f, axis=0 , norm='ortho'), axis=1 , norm='ortho')
+
+#img = Image.open("img/bird.tif")
+
+I = plt.imread("img/cameraman.png")
+
+print('Data type: '+str(I.dtype))
+print('Pixel intensity range: (%d,%d)'%(I.min(),I.max()))
+print(I.shape)
+
+plt.figure(figsize=(10,10))
+plt.imshow(I, cmap = "gray")
+plt.show()
+
+dct_basis = np.zeros((64,64))
+k = 0
+for i in range(8):
+    for j in range(8):
+        g = np.zeros((8,8))
+        g[i,j]=1
+        dct_basis[k,:] = idct2(g).flatten()
+        k+=1
+
+dct_basis = dct_basis/np.max(np.absolute(dct_basis))
+gl.utils.image_grid(dct_basis,n_rows=8,n_cols=8)
+
+patch_size = 8  #8x8 patch
+I_dct = np.zeros_like(I)
+for i in range(0,I.shape[0],patch_size):
+    for j in range(0,I.shape[1],patch_size):
+        I_dct[i:(i+patch_size),j:(j+patch_size)] = dct2(I[i:(i+patch_size),j:(j+patch_size)])
+
+plt.figure(figsize=(10,10))
+plt.imshow(I_dct,cmap='gray',vmin=0,vmax=np.max(I_dct)*0.01)
+plt.show()
+
+#thresh = 0.1
+#I_thresh = I_dct * (np.absolute(I_dct) > thresh*np.max(np.absolute(I_dct)))
+
+patch_size = 8  #8x8 patch
+I_comp = np.zeros_like(I)
+for i in range(0,I.shape[0],patch_size):
+    for j in range(0,I.shape[1],patch_size):
+        I_comp[i:(i+patch_size),j:(j+patch_size)] = idct2(I_dct[i:(i+patch_size),j:(j+patch_size)])
+
+#Plot decompressed (reconstructed image)
+plt.figure(figsize=(30,10))
+plt.imshow(I_comp,cmap='gray')
+plt.imshow(np.hstack((I,I_comp, I-I_comp+0.5)), cmap='gray', vmin=0, vmax=1)
+plt.show()
--- tp1.py
+++ tp1.py
@@ -1,21 +1,38 @@
 from PIL import Image
 import numpy as np
+from scipy.fftpack import dct, idct
 
 
 def encode(path):   
     img = Image.open(path)
-
+    img.show()
     img_array = np.array(img)
     block_img = np.zeros(img_array.shape)
     img_h, img_w = img_array.shape[:2]
     bl_h, bl_w = 8, 8
-    for row in np.arange(img_h - bl_h + 1, step=bl_h):
-        for col in np.arange(img_w - bl_w + 1, step=bl_w):
-            dct_transform(img_array[row:row+bl_h, col:col+bl_w])
+    img_array = dct_transform(img_array)
 
-    print(img_array)
-    #Image.fromarray(imgarray)
 
+    #for row in np.arange(img_h - bl_h + 1, step=bl_h):
+    #    for col in np.arange(img_w - bl_w + 1, step=bl_w):
+    #        tmp = dct_transform(img_array[row:row+bl_h, col:col+bl_w])
+    #        img_array[row:row+bl_h, col:col+bl_w] = tmp
+    #
+    #img_dct = Image.fromarray(img_array)
+    #img_dct.show()
+#
+    #for row in np.arange(img_h - bl_h + 1, step=bl_h):
+    #    for col in np.arange(img_w - bl_w + 1, step=bl_w):
+    #        tmp = idct_transform(img_array[row:row+bl_h, col:col+bl_w])
+    #        img_array[row:row+bl_h, col:col+bl_w] = tmp
+    #
+    #img2 = Image.fromarray(img_array)
+    #img2.show()
+
+def decode()
 
 def dct_transform(block):
-    print(block)
+    return dct(dct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
+
+def idct_transform(block):
+    return idct(idct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
