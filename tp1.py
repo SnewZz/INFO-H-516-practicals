b4eb5c145dee430cf8d556e2bb5c212a7f1501cf
SnewZz <64595565+SnewZz@users.noreply.github.com>
2023-04-24 15:22:41 +0200

    Set up environment

--- tp1.py
+++ tp1.py
@@ -12,25 +12,6 @@ def encode(path):
     bl_h, bl_w = 8, 8
     img_array = dct_transform(img_array)
 
-
-    #for row in np.arange(img_h - bl_h + 1, step=bl_h):
-    #    for col in np.arange(img_w - bl_w + 1, step=bl_w):
-    #        tmp = dct_transform(img_array[row:row+bl_h, col:col+bl_w])
-    #        img_array[row:row+bl_h, col:col+bl_w] = tmp
-    #
-    #img_dct = Image.fromarray(img_array)
-    #img_dct.show()
-#
-    #for row in np.arange(img_h - bl_h + 1, step=bl_h):
-    #    for col in np.arange(img_w - bl_w + 1, step=bl_w):
-    #        tmp = idct_transform(img_array[row:row+bl_h, col:col+bl_w])
-    #        img_array[row:row+bl_h, col:col+bl_w] = tmp
-    #
-    #img2 = Image.fromarray(img_array)
-    #img2.show()
-
-def decode()
-
 def dct_transform(block):
     return dct(dct(block, axis=0, norm='ortho'), axis=1, norm='ortho')
 
