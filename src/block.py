e2ebf382a99442c9d64fde0988d60110cf300e99
SnewZz <64595565+SnewZz@users.noreply.github.com>
2023-05-11 10:39:42 +0200

    End dct + quantification

--- src/block.py
+++ src/block.py
@@ -1,5 +1,15 @@
 from scipy.fftpack import dct, idct
 import numpy as np
+import cv2
+
+QUANTIZATION_MATRIX = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
+                               [12, 12, 14, 19, 26, 58, 60, 55],
+                               [14, 13, 16, 24, 40, 57, 69, 56],
+                               [14, 17, 22, 29, 51, 87, 80, 62],
+                               [18, 22, 37, 56, 68, 109, 103, 77],
+                               [24, 35, 55, 64, 81, 104, 113, 92],
+                               [49, 64, 78, 87, 103, 121, 120, 101],
+                               [72, 92, 95, 98, 112, 100, 103, 99]])
 
 class Block:
     def __init__(self, data):
@@ -8,14 +18,30 @@ class Block:
     def dct(self):
         self.data = dct(dct(self.data.T, norm='ortho').T, norm='ortho')
     
-    def quantize(self, quantization_matrix):
-        self.data = np.round(np.divide(self.data, quantization_matrix)).astype(int)
+    def quantize(self):
+        self.data = np.divide(self.data, QUANTIZATION_MATRIX).astype(int)
 
     def idct(self):
         self.data = idct(idct(self.data.T, norm='ortho').T, norm='ortho')
 
-    def dequantize(self, quantization_matrix):
-        self.data = np.multiply(self.data, quantization_matrix)
+    def dequantize(self):
+        self.data = np.multiply(self.data, QUANTIZATION_MATRIX)
 
     def get_data(self):
         return self.data
+    
+    def convert_to(self, dtype, scale=1.0):
+        self.data = cv2.convertScaleAbs(self.data, alpha=scale, beta=0.0)
+
+        if dtype == "float":
+            self.data = self.data.astype(np.float32)
+        elif dtype == "double":
+            self.data = self.data.astype(np.float64)
+        elif dtype == "int":
+            self.data = self.data.astype(np.int32)
+        elif dtype == "uint8":
+            self.data = self.data.astype(np.uint8)
+        elif dtype == "uint16":
+            self.data = self.data.astype(np.uint16)
+        else:
+            raise ValueError(f"Invalid data type: {dtype}")
--- src/image.py
+++ src/image.py
@@ -74,36 +74,25 @@ class Image:
         return block_array
         
     def encode(self):
+        print("Compression of the image...")
 
-        quantization_matrix = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
-                               [12, 12, 14, 19, 26, 58, 60, 55],
-                               [14, 13, 16, 24, 40, 57, 69, 56],
-                               [14, 17, 22, 29, 51, 87, 80, 62],
-                               [18, 22, 37, 56, 68, 109, 103, 77],
-                               [24, 35, 55, 64, 81, 104, 113, 92],
-                               [49, 64, 78, 87, 103, 121, 120, 101],
-                               [72, 92, 95, 98, 112, 100, 103, 99]])
-
+        # for row_blocks in self.blocks:
+            # for block in row_blocks:
+                # block.dct()
         for row_blocks in self.blocks:
             for block in row_blocks:
+                block.convert_to("double")
                 block.dct()
-                block.quantize(quantization_matrix)
+                #block.quantize()
 
     def decode(self):
-
-        quantization_matrix = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
-                               [12, 12, 14, 19, 26, 58, 60, 55],
-                               [14, 13, 16, 24, 40, 57, 69, 56],
-                               [14, 17, 22, 29, 51, 87, 80, 62],
-                               [18, 22, 37, 56, 68, 109, 103, 77],
-                               [24, 35, 55, 64, 81, 104, 113, 92],
-                               [49, 64, 78, 87, 103, 121, 120, 101],
-                               [72, 92, 95, 98, 112, 100, 103, 99]])
+        print("Decompression of the image...")
 
         for row_blocks in self.blocks:
             for block in row_blocks:
+                #block.dequantize()
                 block.idct()
-                block.dequantize(quantization_matrix)
+                block.convert_to("uint8")
 
     def save_image(self, file_path):
 
@@ -116,7 +105,10 @@ class Image:
             for j in range(num_blocks_w):
                 block = self.blocks[i][j]
                 y, x = i*BLOCK_SIZE, j*BLOCK_SIZE
-                reconstructed[y:y+BLOCK_SIZE, x:x+BLOCK_SIZE] = block.get_data().squeeze().astype(np.uint8)
+                # if(self.channel_count == 1):
+                    # reconstructed[y:y+BLOCK_SIZE, x:x+BLOCK_SIZE] = block.get_data()[:,:, 0]
+                # else:
+                reconstructed[y:y+BLOCK_SIZE, x:x+BLOCK_SIZE] = block.get_data()
 
         # Save the image to file
         img = I.fromarray(reconstructed)
